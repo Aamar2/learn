74cba28362cd0ce1ca17f6bc08e8e705be5da47b
Aamar2 <amar.vallapreddy@gmail.com>
2024-11-11 12:13:27 +0530

    Delete terra directory

--- terra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "value1" {
-      value = module.test_module.apply_format
-}
-
-output "value2" {
-      value = module.test_module.apply_format2
-}
--- terra/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "oci" {
-  auth = "SecurityToken"
-  config_file_profile = "test"
-  region = "us-phoenix-1"
-}
-
-module "test_module" {
-  source = "./modules/test"
-}
